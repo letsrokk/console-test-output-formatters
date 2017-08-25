36383980089dcbb97c29e0a87fec18efc1964291
Mayer Dmitriy Yakovlevich <majer-dy@fxclub.org>
2017-08-25 10:37:29 +0200

    added example tests to display output migrating gradle to maven

---
+++ src/test/java/org/fclub/qa/cucumber/examples/steps/ContextSteps1.java
@@ -0,0 +1,23 @@
+package org.fclub.qa.cucumber.examples.steps;
+
+import cucumber.api.java.en.When;
+import org.fxclub.qa.cucumber.TestContext;
+
+/**
+ * Created by majer-dy on 19/04/2017.
+ */
+public class ContextSteps1 {
+
+    @When("^when in first class we create Helper instance$")
+    public void whe_run_the_scenario() throws Throwable {
+        HelperClass helper = new HelperClass();
+        TestContext.set(helper);
+    }
+
+    @When("^when in first class we create object$")
+    public void when_we_create_object() throws Throwable {
+        HelperClass helper = new HelperClass();
+        TestContext.set("object", helper);
+    }
+
+}
