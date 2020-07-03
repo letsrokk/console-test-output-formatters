05d78995aa045d21038b883673a07c19f59b5c9c
Dmitry Mayer <dmitry.mayer@libertexgroup.com>
2020-07-03 11:42:30 +0200

    replace EXTENDS with IMPLEMETS

--- testng-console-listener/src/main/java/com/github/letsrokk/testng/TestNGConsoleProgressListener.java
+++ testng-console-listener/src/main/java/com/github/letsrokk/testng/TestNGConsoleProgressListener.java
@@ -2,48 +2,49 @@ package com.github.letsrokk.testng;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.testng.IConfigurationListener2;
 import org.testng.ITestContext;
+import org.testng.ITestListener;
 import org.testng.ITestResult;
-import org.testng.TestListenerAdapter;
 
 import java.util.concurrent.TimeUnit;
 
-public class TestNGConsoleProgressListener extends TestListenerAdapter {
+public class TestNGConsoleProgressListener implements ITestListener, IConfigurationListener2 {
 
-    private Logger logger = LogManager.getLogger(TestNGConsoleProgressListener.class);
+    final private Logger logger = LogManager.getLogger(TestNGConsoleProgressListener.class);
 
     @Override
     public void onTestStart(ITestResult tr) {
-        super.onTestStart(tr);
-
         logger.debug("=========================================================");
         logger.debug("=== TESTNG: ON TEST START: " + tr.toString());
         logger.debug("=========================================================");
-
         int testCounter = increaseTestCounter();
         logger.info("START #" + testCounter + ": " + generateMethodSignature(tr));
     }
 
     @Override
     public void onTestSuccess(ITestResult tr) {
-        super.onTestSuccess(tr);
-
         logger.debug("=========================================================");
         logger.debug("=== TESTNG: ON TEST SUCCESS: " + tr.toString());
         logger.debug("=========================================================");
-
         int testCounter = increaseTestFinishedCounter();
         logger.info("SUCCESS #" + testCounter + ": " + generateMethodSignature(tr));
     }
 
     @Override
     public void onTestFailure(ITestResult tr) {
-        super.onTestFailure(tr);
+        logTestFailure(tr);
+    }
 
+    @Override
+    public void onTestFailedButWithinSuccessPercentage(ITestResult tr) {
+        logTestFailure(tr);
+    }
+
+    private void logTestFailure(ITestResult tr) {
         logger.debug("=========================================================");
         logger.debug("=== TESTNG: ON TEST FAILURE: " + tr.toString());
         logger.debug("=========================================================");
-
         int testCounter = increaseTestFinishedCounter();
         String error_msg = Functions.extractMessageError(tr.getThrowable());
         logger.error("FAIL  #" + testCounter + ": " + generateMethodSignature(tr) + ": " + error_msg);
@@ -51,20 +52,25 @@ public class TestNGConsoleProgressListener extends TestListenerAdapter {
 
     @Override
     public void onTestSkipped(ITestResult tr) {
-        super.onTestSkipped(tr);
-
         logger.debug("=========================================================");
         logger.debug("=== TESTNG: ON TEST SKIPPED: " + tr.toString());
         logger.debug("=========================================================");
-
         int testCounter = increaseTestFinishedCounter();
         logger.warn("SKIP  #" + testCounter + ": " + generateMethodSignature(tr));
     }
 
     @Override
-    public void onFinish(ITestContext testContext) {
-        super.onFinish(testContext);
+    public void onStart(ITestContext iTestContext) {
+        logger.debug("=========================================================");
+        logger.debug("=== TESTNG: ON START CALL");
+        long startTimer = System.currentTimeMillis();
+        long seconds = TimeUnit.SECONDS.convert(System.currentTimeMillis() - startTimer, TimeUnit.MILLISECONDS);
+        logger.debug("=== " + seconds + "s");
+        logger.debug("=========================================================");
+    }
 
+    @Override
+    public void onFinish(ITestContext testContext) {
         logger.debug("=========================================================");
         logger.debug("=== TESTNG: ON FINISH CALL");
         long startTimer = System.currentTimeMillis();
@@ -93,7 +99,6 @@ public class TestNGConsoleProgressListener extends TestListenerAdapter {
 
     @Override
     public void beforeConfiguration(ITestResult tr) {
-        super.beforeConfiguration(tr);
         logger.debug("=========================================================");
         logger.debug("=== TESTNG: BEFORE CONFIGURATION: " + tr.toString());
         logger.debug("=========================================================");
@@ -101,7 +106,6 @@ public class TestNGConsoleProgressListener extends TestListenerAdapter {
 
     @Override
     public void onConfigurationSuccess(ITestResult tr) {
-        super.onConfigurationSuccess(tr);
         logger.debug("=========================================================");
         logger.debug("=== TESTNG: CONFIGURATION SUCCESS: " + tr.toString());
         logger.debug("=========================================================");
@@ -109,7 +113,6 @@ public class TestNGConsoleProgressListener extends TestListenerAdapter {
 
     @Override
     public void onConfigurationFailure(ITestResult tr) {
-        super.onConfigurationFailure(tr);
         logger.error("CONFIGURATION FAILURE: " + tr.getName() + ": " + tr.getThrowable().getMessage());
         logger.debug("=========================================================");
         logger.debug("=== TESTNG: CONFIGURATION FAILURE: " + tr.toString());
@@ -118,7 +121,6 @@ public class TestNGConsoleProgressListener extends TestListenerAdapter {
 
     @Override
     public void onConfigurationSkip(ITestResult tr) {
-        super.onConfigurationSkip(tr);
         logger.warn("CONFIGURATION SKIPPED: " + tr.getName());
         logger.debug("=========================================================");
         logger.debug("=== TESTNG: CONFIGURATION SKIP: " + tr.toString());
